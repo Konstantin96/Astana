ebfce96767ec19727061eeadafc2d52760468c0c
ГерценЕ <@228_TEACHER>
2018-07-02 12:00:36 +0600

    Add project files.

---
+++ Astana.DAL/Model/AstanaEntity.cs
@@ -0,0 +1,31 @@
+﻿using Astana.BLL.ModelClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astana.DAL.Model
+{
+    public class AstanaEntity
+    {
+        public List<Client> GetClients()
+        {
+            Client client1 = new Client();
+            client1.login = "admin";
+            client1.password = "admin";
+
+            Client client2 = new Client()
+            { login = "user", password = "user" };
+
+            Client client3 = new Client("god", "god");
+
+            List<Client> clients = new List<Client>();
+            clients.Add(client1);
+            clients.Add(client2);
+            clients.Add(client3);
+            return clients;
+        }
+    }
+    
+}
---
+++ Astana.Web/Program.cs
@@ -0,0 +1,22 @@
+﻿using Astana.BLL.ModelClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astana.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static List<Client> GetClients()
+        {
+            Astana.DAL.Model.AstanaEntity db = new DAL.Model.AstanaEntity();
+            return db.GetClients();
+        }
+
+    }
+}

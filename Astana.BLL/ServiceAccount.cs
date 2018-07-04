e555faaead7458b6fcd5ac9e5934f255bd5c4487
Клюкинк <@228_7>
2018-07-04 11:53:31 +0600

    11

--- Astana.BLL/ServiceAccount.cs
+++ Astana.BLL/ServiceAccount.cs
@@ -13,21 +13,23 @@ namespace Astana.BLL
         private Random rnd = new Random();
         public bool CreateAccount(out string Massage, ref Client client)
         {
-            Account account = new Account();
-            account.AccountType = AccountType.loan;
-            account.Balance = 0;
-            account.Currancy = Currancy.KZT;
-            account.NoAccount = string.Format("{0}{1}", account.Currancy.ToString(), rnd.Next());
-
-            client.accounts.Add(account);
+           
 
             if ((rnd.Next(1, 10) % 2) == 0)
             {
                 Massage = "OK";
+                Account account = new Account();
+                account.AccountType = AccountType.loan;
+                account.Balance = 0;
+                account.Currancy = Currancy.KZT;
+                account.NoAccount = string.Format("{0}{1}", account.Currancy.ToString(), rnd.Next());
+
+                client.accounts.Add(account);
                 return true;
             }
             else
             {
+
                 throw new Exception(message: "Ошибка при открытии счета");
             }
         }
@@ -46,6 +48,36 @@ namespace Astana.BLL
                 return false;
             }
                 
+        }
+        public bool DelBalance(out string Massage, ref Client client, string NoAccount ,double Summ)
+        {
+
+            Account acc = client.accounts.FirstOrDefault(f => f.NoAccount == NoAccount);
+            if (acc != null)
+            {
+                if (acc.Balance <= 0)
+                {
+                    Massage = "На счете не достаточно средств!";
+                    return false;
+                }
+                else if (acc.Balance > 0 && acc.Balance - Summ < 0)
+                {
+                    Massage = "На счете не достаточно средств!";
+                    return false;
+                }
+                else
+                {
+                    acc.Balance -= Summ;
+                    Massage = "Успешно";
+                }
+                return true;
+            }
+            else
+            {
+                Massage = "У вас не достаточно денег на счете!";
+                return false;
+            }
+
         }
     }
 }
--- Astana.Web/Program.cs
+++ Astana.Web/Program.cs
@@ -18,86 +18,116 @@ namespace Astana.Web
 
         static void Main(string[] args)
         {
-            Console.Write("Введите логин: ");
-            string login = Console.ReadLine();
-            Console.Write("Введите пароль: ");
-            string password = Console.ReadLine();
-            client = db.GetClients()
-                .FirstOrDefault(fd => fd.login == login
-                && fd.password == password);
+            int popytka = 0;
+            int sumpop = 3;
 
-
-
-            if (client != null)
+            do
             {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                string password = Console.ReadLine();
+                client = db.GetClients()
+                    .FirstOrDefault(fd => fd.login == login
+                    && fd.password == password);
 
-                Console.Clear();
-                Console.WriteLine("Добро пожаловать {0}", client.fullName);
-                char ent;
-                do
+                if (client != null)
                 {
-                    Menu.GetMenu();
-                    ent = Console.ReadLine().ToLower()[0];
-                    switch (ent)
-                    {
-                        case 'a':
-                            {
-                                string masage = "";
-                                ServiceAccount SA = new ServiceAccount();
-                                try
-                                {
-                                    SA.CreateAccount(out masage, ref client);
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.WriteLine("Счет успешно открыт!");
-                                    Console.ForegroundColor = ConsoleColor.Gray;
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine(ex.Message);
-                                    Console.ForegroundColor = ConsoleColor.Gray;
-                                }
-                            }
-                            break;
-                        case 'b':
-                            {
-                                foreach (Account item in client.accounts)
-                                {
-                                    Console.WriteLine("{0}\t{1} {2}", item.NoAccount, item.Balance, item.Currancy);
-                                }
-                            }
-                            break;
-                        case 'c':
-                            {
-                                string mas = "";
-                                string NoAcc = "";
-                                double Summ = 0;
 
-                                Console.Write("Введите номер счета: ");
-                                NoAcc = Console.ReadLine();
-                                Console.Write("Введите cумму пополнения: ");
-                                Summ = double.Parse(Console.ReadLine());
-
-                                ServiceAccount SB = new ServiceAccount();
-                                SB.SetBalance(out mas, ref client, NoAcc, Summ);
-                                
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-
-                    Console.ReadLine();
                     Console.Clear();
-                } while (ent!='e');
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Не правильный логин или пароль, попробуйте еще раз");
-            }
+                    Console.WriteLine("Добро пожаловать {0}", client.fullName);
+                    char ent;
+                    do
+                    {
+                        Menu.GetMenu();
+                        ent = Console.ReadLine().ToLower()[0];
+                        switch (ent)
+                        {
+                            case 'a':
+                                {
+                                    string masage = "";
+                                    ServiceAccount SA = new ServiceAccount();
+                                    try
+                                    {
+                                        SA.CreateAccount(out masage, ref client);
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        Console.WriteLine("Счет успешно открыт!");
+                                        Console.ForegroundColor = ConsoleColor.Gray;
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine(ex.Message);
+                                        Console.ForegroundColor = ConsoleColor.Gray;
+                                    }
+                                }
+                                break;
+                            case 'b':
+                                {
+                                    foreach (Account item in client.accounts)
+                                    {
+                                        Console.WriteLine("{0}\t{1} {2}", item.NoAccount, item.Balance, item.Currancy);
+                                    }
+                                }
+                                break;
+                            case 'c':
+                                {
+                                    string mas = "";
+                                    string NoAcc = "";
+                                    double Summ = 0;
 
+                                    Console.Write("Введите номер счета: ");
+                                    NoAcc = Console.ReadLine();
+                                    Console.Write("Введите cумму пополнения: ");
+                                    Summ = double.Parse(Console.ReadLine());
 
+                                    ServiceAccount SB = new ServiceAccount();
+                                    SB.SetBalance(out mas, ref client, NoAcc, Summ);
+                                    Console.WriteLine(mas);
+
+                                }
+                                break;
+                            case 'd':
+                                {
+                                    string mas = "";
+                                    string NoAcc = "";
+                                    double Summ = 0;
+                                    Console.Write("Введите номер счета: ");
+                                    NoAcc = Console.ReadLine();
+                                    Console.Write("Введите cумму которую хотите снять: ");
+                                    Summ = double.Parse(Console.ReadLine());
+
+                                    ServiceAccount SB = new ServiceAccount();
+                                    SB.DelBalance(out mas, ref client, NoAcc, Summ);
+                                    Console.WriteLine(mas);
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("Вы ввели не правильное значение!");
+                                break;
+                        }
+
+                        Console.ReadLine();
+                        Console.Clear();
+                    } while (ent != 'e');
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Не правильный логин или пароль, попробуйте еще раз");
+                    sumpop = sumpop - 1;
+                    if (sumpop == 2)
+                        Console.WriteLine("Осталось {0} попытки", sumpop);
+                    else if (sumpop == 1)
+                        Console.WriteLine("Осталось {0} попыткa", sumpop);
+                    popytka++;
+
+                    if (popytka == 3)
+                    {
+                        Console.WriteLine("Банкинг заблкирован!");
+                    }
+                }
+            } while (popytka != 3);
         }
 
     }
